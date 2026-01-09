6471ab43bd3da1b8bcff78ee76774dffb2499cd4
3doriTea <sorasora3program@gmail.com>
2026-01-09 18:11:47 +0900

    first server

--- VRTea/Main.cpp
+++ VRTea/Main.cpp
@@ -1,2 +1,21 @@
 #include <DxLib.h>
 
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	if (DxLib_Init() == -1)
+	{
+		return -1;
+	}
+
+	while (CheckHitKeyAll() == 0)
+	{
+		if (ProcessMessage() == -1)
+		{
+			break;
+		}
+	}
+
+	DxLib_End();
+
+	return 0;
+}
