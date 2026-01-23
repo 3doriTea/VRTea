2d33c1d88b4c6b053ac88f7e7815970a80941612
YandereFox96 <kurokitune-gingitune3@au.com>
2026-01-23 17:28:42 +0900

    PlayerDataのjson変換多分できた

--- VRTea/PlayerData.h
+++ VRTea/PlayerData.h
@@ -6,3 +6,14 @@ struct PlayerData
 	DxLib::COLOR_U8 color;
 	std::string name;
 };
+
+namespace ns {
+    void to_json(json& j, const PlayerData& p) {
+        j = json{{"name", p.name}, {"color", p.color}};
+    }
+
+    void from_json(const json& j, PlayerData& p) {
+        j.at("name").get_to(p.name);       // get_to(T& arg) ‚Í arg = get<T>() ‚Æ“¯‚¶
+        j.at("color").get_to(p.color);
+    }
+}
