db8bfd7a5b7174755ca533c80fcbb0d21cb16361
nishi-0831 <240050@jc-21.jp>
2026-01-23 16:39:11 +0900

    PlayerDataとPlayerState構造体を作成

---
+++ VRTea/PlayerData.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <DxLib.h>
+#include <string>
+struct PlayerData
+{
+	DxLib::COLOR_U8 color;
+	std::string name;
+};
---
+++ VRTea/PlayerState.h
@@ -0,0 +1,6 @@
+#pragma once
+#include <DxLib.h>
+struct PlayerState
+{
+	DxLib::VECTOR position;
+};
