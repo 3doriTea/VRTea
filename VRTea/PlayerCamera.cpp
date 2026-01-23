114960fede96ab19fdd047488003b2055f8ba3ac
240145 <240145@E0403-05>
2026-01-23 17:31:05 +0900

    PlayerCameraの作成

---
+++ VRTea/PlayerCamera.cpp
@@ -0,0 +1,14 @@
+#include "PlayerCamera.h"
+
+void PlayerCamera::Update()
+{
+	SetCameraPositionAndTargetAndUpVec();
+}
+
+void PlayerCamera::Draw()
+{
+}
+
+void PlayerCamera::SetPosition(VECTOR pos)
+{
+}
