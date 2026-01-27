cd342f64396cf6b800942c5f62622d5d82ac4e2d
240145 <240145@E0403-05>
2026-01-27 16:49:02 +0900

    カメラの位置をカプセルが見える位置に移動した

--- VRTea/PlayerCamera.cpp
+++ VRTea/PlayerCamera.cpp
@@ -14,7 +14,7 @@ PlayerCamera::~PlayerCamera()
 void PlayerCamera::Update()
 {
 	//SetCameraPositionAndTargetAndUpVec(VGet(0.0f,0.0f,-10.0f),VGet(0.0f, 0.0f,0.0f),VGet(0.0f,1.0f,0.0f));
-	SetCameraPositionAndTarget_UpVecY(VGet(0.0f, 0.0f, -10.0f), VGet(0.0f, 0.0f, 0.0f));
+	SetCameraPositionAndTarget_UpVecY(VGet(0.0f, 0.0f, -100.0f), VGet(0.0f, 0.0f, 0.0f));
 }
 
 void PlayerCamera::Draw()
@@ -22,7 +22,7 @@ void PlayerCamera::Draw()
 	DrawCapsule3D(VGet(0.0f,0.0f,0.0f),VGet(0.0f,10.0f,0.0f),2.0f,20.0f,GetColor(255,0,0),GetColor(255,255,255),TRUE);
 	DrawCircle(100, 100, 10.0f, GetColor(0, 255, 0), TRUE);
 
-	DrawCapsule3D(VGet(0.0f, 0.0f, 0.0f), VGet(0.0f, 10.0f, 0.0f), 40.0f, 8, GetColor(0, 255, 0), GetColor(255, 255, 255), TRUE);
+	//DrawCapsule3D(VGet(0.0f, 0.0f, 0.0f), VGet(0.0f, 10.0f, 0.0f), 40.0f, 8, GetColor(0, 255, 0), GetColor(255, 255, 255), TRUE);
 }
 
 void PlayerCamera::SetPosition(VECTOR pos)
