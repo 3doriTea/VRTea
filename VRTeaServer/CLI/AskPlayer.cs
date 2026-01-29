018fda44aecb67629c556606008b6f589dcb8c34
3doriTea <sorasora3program@gmail.com>
2026-01-29 17:49:12 +0900

    CLI質問機能実装

---
+++ VRTeaServer/CLI/AskPlayer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VRTeaServer.CLI
+{
+	/// <summary>
+	/// 質問を再生するやつ
+	/// </summary>
+	internal class AskPlayer
+	{
+		private List<AskBase> _asks;
+
+		public AskPlayer(List<AskBase> asks)
+		{
+			_asks = asks;
+		}
+
+		/// <summary>
+		/// 質問を再生する
+		/// </summary>
+		public void Play()
+		{
+			foreach (AskBase ask in _asks)
+			{
+				ask.Ask();
+			}
+		}
+	}
+}
--- VRTeaServer/Program.cs
+++ VRTeaServer/Program.cs
@@ -1,10 +1,31 @@
-﻿namespace VRTeaServer
+﻿using VRTeaServer.CLI;
+
+namespace VRTeaServer
 {
 	internal class Program
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, World!");
+			var askIPAddress = new AskIPAddress();
+			var askPortNumber = new AskPortNumber();
+			var askReady = new AskReady();
+
+			var askPlayer = new AskPlayer(
+			[
+				new AskLine(),
+				askIPAddress,
+				askPortNumber,
+				askReady,
+			]);
+
+			while (!askReady.Ready)
+			{
+				askPlayer.Play();
+			}
+
+			Console.Write($"Booting...");
+			Console.Write($"\"{askIPAddress.IPAddress}:{askPortNumber.PortNumber}\"...");
+			Console.WriteLine($"Ok!");
 		}
 	}
 }
