433b798c6d43c758255bda170f0cafbd62bbfdf6
nishi-0831 <240050@jc-21.jp>
2026-01-30 16:05:56 +0900

    Chatがチャットのログを表示するようになった。いったんNetQueueStubから読み取っている Tabキーでログの開閉ができる

--- VRTea/Chat.cpp
+++ VRTea/Chat.cpp
@@ -1,17 +1,48 @@
-#include <vector>
-#include <string>
+#include <nlohmann/json.hpp>
+#include <format>
+#include "../ImGui/imgui.h"
 #include "Chat.h"
-
+#include "NetQueue.h"
+#include "NetQueueStub.h"
+Chat::Chat()
+	: showLogWindow_{true}
+{
+}
 void Chat::Update()
 {
+	ReadContent();
+
+	if (ImGui::IsKeyPressed(ImGuiKey_Tab))
+	{
+		showLogWindow_ = !showLogWindow_;
+	}
 }
 
 void Chat::Draw()
 {
+	if (showLogWindow_)
+	{
+		DrawChatLog();
+	}
 }
 
 void Chat::DrawChatLog()
 {
+	ImGui::Begin("Log", 0, ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoResize);
+
+	for (const std::string& text : chatLog_)
+	{
+		ImGui::Text("%s", text.c_str());
+		ImGui::Separator();
+	}
+	
+	// 既に一番下にスクロールしている場合
+	if (ImGui::GetScrollY() >= ImGui::GetScrollMaxY())
+	{
+		ImGui::SetScrollHereY(1.0f);
+	}
+
+	ImGui::End();
 }
 
 void Chat::DrawInputField()
@@ -22,4 +53,29 @@ void Chat::DrawInputField()
 void Chat::InputText()
 {
 	
+}
+
+void Chat::ReadContent()
+{
+	using namespace nlohmann;
+
+	NetQueueStub* netQueue = FindGameObject<NetQueueStub>();
+	if (netQueue == nullptr)
+		return;
+	std::string str = netQueue->Read();
+	while (str != "")
+	{
+		json j = json::parse(str);
+		// Headの存在をチェック
+		if (j.contains("Head") == false)
+			return;
+
+		std::string head = j.at("Head").get<std::string>();
+		std::string content = j.at("Content").get<std::string>();
+
+		std::string log = std::format("Event:{} Content:{}", head, content);
+		chatLog_.push_back(log);
+
+		str = netQueue->Read();
+	}
 }
--- VRTea/Chat.h
+++ VRTea/Chat.h
@@ -1,11 +1,25 @@
 #pragma once
 #include "GameObject.h"
-
+#include <vector>
+#include <string>
 struct Chat : public GameObject
 {
+	Chat();
 	void Update() override;
 	void Draw() override;
+	/// <summary>
+	/// チャットのログを表示する
+	/// </summary>
 	void DrawChatLog();
 	void DrawInputField();
 	void InputText();
+	/// <summary>
+	/// NetQueueからチャット内容を読み取る
+	/// </summary>
+	void ReadContent();
+private:
+	// チャットのログを保存するコンテナ
+	std::vector<std::string> chatLog_;
+	// チャットのログ開閉フラグ
+	bool showLogWindow_;
 };
