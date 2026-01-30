f7783d1dcbd87f2bc4d7be4430bf190c80ae49c1
REIREIREIMU <240255jc-21.jp>
2026-01-30 11:32:12 +0900

    ノンブロッキング設定作成、コンストラクタの中身の処理と送信キューを作成

--- VRTea/NetQueue.cpp
+++ VRTea/NetQueue.cpp
@@ -1,32 +1,71 @@
 #include "NetQueue.h"
+#include <winsock2.h>  
 
-void NetQueue::Send(std::string content)
+// ノンブロッキング設定(そのまま次の処理に行く用)
+void NetQueue::SetNonBlocking(SOCKET S)
 {
-    sendQueue.push(content);
+    u_long mode = 1; 
+    ioctlsocket(S, FIONBIO, &mode);
 }
 
-std::string NetQueue::Read()
+// コンストラクタ(ここは前回のネットワークの物を引用)
+NetQueue::NetQueue()
+{
+    // WinSock2.2 初期化
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+    {
+        std::cout << "Error : WSAStartup" << std::endl;
+        return;
+    }
+    std::cout << "Success : WSAStartup" << std::endl;
+
+    //リスンソケットの作成(前回の応用)
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock == INVALID_SOCKET)
+    {
+        std::cout << "Error : socket" << std::endl;
+        WSACleanup();
+        return;
+    }
+    std::cout << "Success : socket" << std::endl;
+
+    // ノンブロッキング処理
+    SetNonBlocking(sock);
+
+}
+
+// デストラクタ
+NetQueue::~NetQueue()
+{
+    // ソケットを閉じてWSACleanup
+    if (sock != INVALID_SOCKET)
+    {
+        closesocket(sock);
+        sock = INVALID_SOCKET;
+    }
+    WSACleanup();
+}
+
+// 送信キューに積む
+void NetQueue::Send(const std::string& content)
+{
+    // TCPのメッセージ境界を作るために、改行
+    sendQueue.push(content + "\n");
+}
+
+bool NetQueue::Connect(const char* ip, uint16_t port)
+{
+    return false;
+}
+
+// 受信キューから1件取り出す処理
+std::string NetQueue::Read(std::string& out)
 {
     std::string Q = readQueue.front();
     readQueue.pop();
 
     return Q;
-}
-
-NetQueue::NetQueue()
-{
-    SOCKET sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock == INVALID_SOCKET)
-    {
-        WSACleanup();
-        return;
-    }
-    set_nonblocking(sock);
-}
-
-NetQueue::~NetQueue()
-{
-
 }
 
 void NetQueue::Update()
@@ -39,7 +78,7 @@ void NetQueue::Update()
         0);                   
 }
 
-void NetQueue::Drow()
+void NetQueue::Draw()
 {
 
 }
